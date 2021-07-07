185327020068de62387bdae40115d3049148cc21
Yupei Yang <yupei.yang@oracle.com>
2021-07-07 12:34:19 -0400

    CAAS-926 update datasources.tf and description of instance_image_id

--- datasources.tf
+++ datasources.tf
@@ -1,7 +1,35 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Get the image id of Oracle Autonomous Linux
+# ---------------------------------------------------------------------------------------------------------------------
 data "oci_core_images" "autonomous_images" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Oracle Autonomous Linux"
   operating_system_version = var.instance_operating_system_version
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Get a list of availability domains
+# ---------------------------------------------------------------------------------------------------------------------
+data "oci_identity_availability_domains" "ad" {
+  compartment_id = var.compartment_ocid
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Bootstrap script and variables
+# ---------------------------------------------------------------------------------------------------------------------
+data "template_file" bootstrap {
+  template = file("${path.module}/userdata/bootstrap")
+
+  vars = {
+    bootstrap_bucket = var.bootstrap_bucket
+    bootstrap_bundle = var.bastion_bootstrap_bundle
+    wazuh_server_ip  = var.wazuh_server_ip
+    playbook_name = var.playbook_name
+  }
+}
+
+data "oci_core_instance" "bastion_host" {
+  instance_id = oci_core_instance.bastion.id
 }
